e8b25e33ad1fcb64a4051970bf4fed76432a5197
louis <louisnguyen.tech@gmail.com>
2023-01-12 09:02:49 +0700

    Fix menu

--- src/layouts/header/View.tsx
+++ src/layouts/header/View.tsx
@@ -9,12 +9,17 @@ import {
   NavLink,
   Container,
 } from "reactstrap";
+import { isMobile } from "react-device-detect";
 
 function Example() {
   const [isOpen, setIsOpen] = useState(false);
   const history = useHistory();
 
-  const toggle = () => setIsOpen(!isOpen);
+  const toggle = () => {
+    if (isMobile) {
+      setIsOpen(!isOpen);
+    }
+  };
 
   const menus = [
     {
@@ -35,6 +40,33 @@ function Example() {
     },
   ];
 
+  const menusMobile = [
+    {
+      label: "自分の記録",
+      link: "",
+    },
+    {
+      label: "体重グラフ",
+      link: "",
+    },
+    {
+      label: "目標",
+      link: "",
+    },
+    {
+      label: "選択中のコース",
+      link: "",
+    },
+    {
+      label: "コラム一覧",
+      link: "",
+    },
+    {
+      label: "設定",
+      link: "",
+    },
+  ];
+
   const redirect = (link) => {
     history.push(link);
   };
@@ -49,23 +81,33 @@ function Example() {
           {/* <NavbarToggler onClick={toggle}  /> */}
           <Collapse isOpen={isOpen} navbar className="menu-item-wrapper">
             <Nav className="" navbar>
-              {menus.map((m) => (
-                <NavItem>
-                  <NavLink onClick={(_) => redirect(m.link)}>
-                    <div className="menu-item">
-                      <img className="menu-item-img" src={m.icon} alt="" />
-                      <span className="menu-item-text">{m.label}</span>
-                      {m.iconInfo && (
-                        <img
-                          className="menu-item-info"
-                          src={m.iconInfo}
-                          alt=""
-                        />
-                      )}
-                    </div>
-                  </NavLink>
-                </NavItem>
-              ))}
+              {isMobile
+                ? menusMobile.map((m) => (
+                    <NavItem>
+                      <NavLink onClick={(_) => redirect(m.link)}>
+                        <div className="menu-item-mobile">
+                          <span className="menu-item-text">{m.label}</span>
+                        </div>
+                      </NavLink>
+                    </NavItem>
+                  ))
+                : menus.map((m) => (
+                    <NavItem>
+                      <NavLink onClick={(_) => redirect(m.link)}>
+                        <div className="menu-item">
+                          <img className="menu-item-img" src={m.icon} alt="" />
+                          <span className="menu-item-text">{m.label}</span>
+                          {m.iconInfo && (
+                            <img
+                              className="menu-item-info"
+                              src={m.iconInfo}
+                              alt=""
+                            />
+                          )}
+                        </div>
+                      </NavLink>
+                    </NavItem>
+                  ))}
             </Nav>
           </Collapse>
           <div className="button-toggle" onClick={(_) => toggle()}>
